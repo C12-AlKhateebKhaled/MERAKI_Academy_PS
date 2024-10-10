c5afc25276f13727cb02b4bd5ade9a04303568a7
AlKhateebKhaled <khaled.hkhateeb@gmail.com>
2024-10-10 19:33:36 +0300

    Q5-W14D03-Main

--- Q5-W14D03-Main/main.js
+++ Q5-W14D03-Main/main.js
@@ -5,8 +5,37 @@ Have the function findIntersection(strArr) read the array of strings stored in s
 */
 
 const findIntersection = (strArr) => {
-  // YOUR CODE HERE
+  /*
+1. Separate strArr to two Strings.
+2. Remove the commas using split(", ").
+3.[filteredArray] : Filter the included numbers, using filter and includes.
+4. 
+  * If there are common numbers , use join to return a comma-separated string.
+  * If there is no intersection, return the string false.
+
+*/
+  const firstArray = strArr[0].split(", ");
+  const secondArray = strArr[1].split(", ");
+
+  const filteredArray = secondArray.filter((element) =>
+    firstArray.includes(element)
+  );
+  const commaSeparatedString = filteredArray.join(", ");
+
+  const result = filteredArray.length ? commaSeparatedString : false;
+
+  console.log(
+    firstArray,
+    secondArray,
+    filteredArray,
+    commaSeparatedString,
+    "Final Result: ",
+    result
+  );
+
+  return result;
 };
+findIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]);
 
 /*
 Examples:
@@ -15,4 +44,5 @@ findIntersection(['1, 3, 9, 10, 17, 18', '1, 4, 9, 10']); // => '1,9,10'
 findIntersection(['2, 5, 7, 14', '1, 4, 13, 15, 25']); // => false
 findIntersection(['', '1, 2, 4, 13, 15']); // => false
 */
+
 module.exports = { findIntersection };
